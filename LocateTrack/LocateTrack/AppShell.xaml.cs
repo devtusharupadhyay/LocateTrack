03db19c9ede9fa3e15e08a30deee1e182fd0e5a2
tusharupadhyay <programmingmaster@outlook.com>
2019-08-16 21:53:00 +0530

    first commit

---
+++ LocateTrack/LocateTrack/AppShell.xaml.cs
@@ -0,0 +1,29 @@
+﻿using LocateTrack.Views;
+using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace LocateTrack
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public static bool IsUserLoggedIn;
+
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigating(ShellNavigatingEventArgs args)
+        {
+            base.OnNavigating(args);
+
+            if (!IsUserLoggedIn)
+            {
+                Navigation.PushModalAsync(new DecisionPage());
+            }
+
+        }
+    }
+}
---
+++ LocateTrack/LocateTrack/Views/DecisionPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LocateTrack.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DecisionPage : ContentPage
+    {
+        public DecisionPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Passenger_Tapped(object sender, EventArgs e)
+        {
+            await PassengerLayout.RelScaleTo(-0.1, 250);
+            await PassengerLayout.RelScaleTo(0.1, 250);
+            await Navigation.PushModalAsync( new Passenger.LoginPage());
+        }
+
+        private async void Pilot_Tapped(object sender, EventArgs e)
+        {
+            await PilotLayout.RelScaleTo(-0.1, 250);
+            await PilotLayout.RelScaleTo(0.1, 250);
+            await Navigation.PushModalAsync(new Pilot.LoginPage());
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            return false;
+        }
+    }
+}
